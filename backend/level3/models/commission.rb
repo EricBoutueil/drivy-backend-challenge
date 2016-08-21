4286b60b6ab7a89343461d49fbc3be5f7df96313
alexandre025 <alexandre.ferraille@gmail.com>
2016-08-21 14:47:18 +0200

    Level 3 complete

---
+++ backend/level3/models/commission.rb
@@ -0,0 +1,21 @@
+class Commission
+
+  attr_accessor :insurance_fee, :assistance_fee, :drivy_fee, :rental
+
+  def initialize(rental)
+    # belongs_to :rental
+    @rental = rental
+  end
+
+  def commission_calculator
+    commission_amount = @rental.price * 0.3
+
+    @insurance_fee = commission_amount * 0.5
+    @assistance_fee = @rental.duration * 100
+    @drivy_fee = commission_amount - @insurance_fee - @assistance_fee
+
+    {insurance_fee: @insurance_fee.to_i, assistance_fee: @assistance_fee.to_i, drivy_fee: @drivy_fee.to_i}
+  end
+
+
+end
