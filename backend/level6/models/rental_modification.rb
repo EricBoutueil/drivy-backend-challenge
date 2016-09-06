998359b65a6380751a00efd051b3db4b3c84ee44
alexandre025 <alexandre.ferraille@gmail.com>
2016-09-06 21:27:01 +0200

    Cleanup rental_modification.rb

--- backend/level6/models/rental_modification.rb
+++ backend/level6/models/rental_modification.rb
@@ -23,44 +23,7 @@ class RentalModification < RentalBase
   end
 
   def new_actions
-    actions = Array.new
-
-    if price > @rental.price
-      # CASE 1 : New price is higher
-      %w( driver owner insurance assistance drivy ).each do |a|
-        type = (a == 'driver') ? 'debit' : 'credit'
-        case a
-          when 'driver'
-            amount = price - @rental.price + deductible_reduction_calculator - @rental.deductible_reduction_calculator
-          when 'owner'
-            amount = -1*(@rental.price*0.7 - price*0.7)
-          when 'drivy'
-            amount = @commission.drivy_fee - @rental.deductible_reduction_calculator - @rental.commission.drivy_fee + deductible_reduction_calculator
-          else
-            amount = @commission.send("#{a}_fee") - @rental.commission.send("#{a}_fee")
-        end
-        actions << {who: a, type: type, amount: amount.to_i}
-      end
-
-    else
-      # CASE 2 : New price is lower
-      %w( driver owner insurance assistance drivy ).each do |a|
-        type = (a == 'driver') ? 'credit' : 'debit'
-        case a
-          when 'driver'
-            amount = @rental.price + @rental.deductible_reduction_calculator - price - deductible_reduction_calculator
-          when 'owner'
-            amount = -1*(price*0.7 - @rental.price*0.7)
-          when 'drivy'
-            amount = @rental.commission.drivy_fee - @commission.drivy_fee + @rental.deductible_reduction_calculator - deductible_reduction_calculator
-          else
-            amount = @rental.commission.send("#{a}_fee") - @commission.send("#{a}_fee")
-        end
-        actions << {who: a, type: type, amount: amount.to_i}
-      end
-
-    end
-    actions
+    price > @rental.price ? debit_driver : refund_driver
   end
 
   def rental_calculator
@@ -69,6 +32,46 @@ class RentalModification < RentalBase
         rental_id: @rental.id,
         actions: new_actions
     }
+  end
+
+  private
+
+  def refund_driver
+    actions = Array.new
+    %w( driver owner insurance assistance drivy ).each do |a|
+      type = (a == 'driver') ? 'credit' : 'debit'
+      case a
+        when 'driver'
+          amount = @rental.price + @rental.deductible_reduction_calculator - price - deductible_reduction_calculator
+        when 'owner'
+          amount = -1*(price*0.7 - @rental.price*0.7)
+        when 'drivy'
+          amount = @rental.commission.drivy_fee - @commission.drivy_fee + @rental.deductible_reduction_calculator - deductible_reduction_calculator
+        else
+          amount = @rental.commission.send("#{a}_fee") - @commission.send("#{a}_fee")
+      end
+      actions << {who: a, type: type, amount: amount.to_i}
+    end
+    actions
+  end
+
+  def debit_driver
+    actions = Array.new
+    %w( driver owner insurance assistance drivy ).each do |a|
+      type = (a == 'driver') ? 'debit' : 'credit'
+      case a
+        when 'driver'
+          amount = price - @rental.price + deductible_reduction_calculator - @rental.deductible_reduction_calculator
+        when 'owner'
+          amount = -1*(@rental.price*0.7 - price*0.7)
+        when 'drivy'
+          amount = @commission.drivy_fee - @rental.deductible_reduction_calculator - @rental.commission.drivy_fee + deductible_reduction_calculator
+        else
+          amount = @commission.send("#{a}_fee") - @rental.commission.send("#{a}_fee")
+      end
+      actions << {who: a, type: type, amount: amount.to_i}
+    end
+    actions
   end
 
 end
